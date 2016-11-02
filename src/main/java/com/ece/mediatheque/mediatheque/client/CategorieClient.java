346f71c22de08117a792ff8564e9924aaf75170a
AurelienGuerard <auguerard@yahoo.fr>
2016-11-02 14:36:06 +0100

    cotcotcot

--- src/main/java/com/ece/mediatheque/mediatheque/client/CategorieClient.java
+++ src/main/java/com/ece/mediatheque/mediatheque/client/CategorieClient.java
@@ -69,7 +69,7 @@ public class CategorieClient implements Serializable {
         }
 
         public void modifierCotisation(double cot) {
-                cotisation = 4; // TODO: lol
+                cotisation = cot; // TODO: lol
         }
 
         public void modifierCoefDuree(double coefDuree) {
